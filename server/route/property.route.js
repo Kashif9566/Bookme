c6dca350be23f60a9400a4ecc6417ab5e436ec2e
Kashif9566 <kashifsaeed9566@gmail.com>
2024-01-11 01:57:15 +0500

    added multer file

--- server/route/property.route.js
+++ server/route/property.route.js
@@ -1,20 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
-const path = require("path");
 const property = require("../controller/property.controller");
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    return cb(null, "./uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
-const upload = multer({ storage: storage });
+const upload = require("../config/multer.config");
+
 router.post(
   "/user/:userId/property",
   upload.single("image"),
--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,20 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
-const path = require("path");
 const user = require("../controller/user.controller");
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    return cb(null, "./uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
-const upload = multer({ storage: storage });
+const upload = require("../config/multer.config");
+
 router.post("/register", upload.single("image"), user.createUser);
 router.post("/login", user.login);
 
