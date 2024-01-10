0d7287c2ff8d059543c013eefd85d17c9e46ccfc
Kashif9566 <kashifsaeed9566@gmail.com>
2024-01-11 01:46:35 +0500

    config multer

--- server/route/property.route.js
+++ server/route/property.route.js
@@ -4,7 +4,9 @@ const multer = require("multer");
 const path = require("path");
 const property = require("../controller/property.controller");
 const storage = multer.diskStorage({
-  destination: "./uploads/",
+  destination: function (req, file, cb) {
+    return cb(null, "./uploads");
+  },
   filename: function (req, file, cb) {
     cb(
       null,
--- server/route/user.route.js
+++ server/route/user.route.js
@@ -4,7 +4,9 @@ const multer = require("multer");
 const path = require("path");
 const user = require("../controller/user.controller");
 const storage = multer.diskStorage({
-  destination: "./uploads/",
+  destination: function (req, file, cb) {
+    return cb(null, "./uploads");
+  },
   filename: function (req, file, cb) {
     cb(
       null,
